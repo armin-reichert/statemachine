4a7e1fb76b9370c817b717650d1d93f927a469a0
Armin Reichert <armin.reichert@web.de>
2019-01-27 20:01:11 +0100

    unit test

--- StateMachine/src/test/java/de/amr/statemachine/junit/StateMachineTest.java
+++ StateMachine/src/test/java/de/amr/statemachine/junit/StateMachineTest.java
@@ -9,21 +9,29 @@ import de.amr.statemachine.StateMachine;
 
 public class StateMachineTest {
 
-	private StateMachine<String, String> fsm;
+	public enum State {
+		S1, S2, S3
+	}
+
+	public enum Event {
+		E1, E2, E3
+	}
+
+	private StateMachine<State, Event> fsm;
 
 	@Before
 	public void buildMachine() {
 		/*@formatter:off*/
-		fsm = StateMachine.beginStateMachine(String.class, String.class, Match.BY_EQUALITY)
-				.initialState("Start")
+		fsm = StateMachine.beginStateMachine(State.class, Event.class, Match.BY_EQUALITY)
+				.initialState(State.S1)
 				.states()
-					.state("Start")
-					.state("Intermediate")
-					.state("Final")
+					.state(State.S1)
+					.state(State.S2)
+					.state(State.S3)
 				.transitions()
-					.when("Start").then("Start").on("E")
-					.when("Start").then("Intermediate").on("F")
-					.when("Intermediate").then("Final").on("G")
+					.when(State.S1).then(State.S1).on(Event.E1)
+					.when(State.S1).then(State.S2).on(Event.E2)
+					.when(State.S2).then(State.S3).on(Event.E3)
 		.endStateMachine();
 		/*@formatter:on*/
 	}
@@ -31,36 +39,35 @@ public class StateMachineTest {
 	@Test
 	public void testInitialization() {
 		fsm.init();
-		Assert.assertEquals("Start", fsm.getState());
+		Assert.assertEquals(State.S1, fsm.getState());
 	}
 
 	@Test(expected = IllegalStateException.class)
 	public void testNoTransition() {
 		fsm.init();
-		fsm.process("Event1");
+		fsm.process(Event.E3);
 	}
 
 	@Test
 	public void testSelfTransition() {
 		fsm.init();
-		fsm.process("E");
-		Assert.assertEquals("Start", fsm.getState());
+		fsm.process(Event.E1);
+		Assert.assertEquals(State.S1, fsm.getState());
 	}
-	
+
 	@Test
 	public void testTransition() {
 		fsm.init();
-		fsm.process("F");
-		Assert.assertEquals("Intermediate", fsm.getState());
+		fsm.process(Event.E2);
+		Assert.assertEquals(State.S2, fsm.getState());
 	}
-	
+
 	@Test
 	public void testThreeTransitions() {
 		fsm.init();
-		fsm.process("E");
-		fsm.process("F");
-		fsm.process("G");
-		Assert.assertEquals("Final", fsm.getState());
+		fsm.process(Event.E1);
+		fsm.process(Event.E2);
+		fsm.process(Event.E3);
+		Assert.assertEquals(State.S3, fsm.getState());
 	}
-
 }
