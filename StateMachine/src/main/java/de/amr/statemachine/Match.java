2abcc316c11a277e6f3f0452c6fdfcbd1bd58d7b
Armin Reichert <armin.reichert@web.de>
2019-01-27 20:18:17 +0100

    javadoc

--- StateMachine/src/main/java/de/amr/statemachine/Match.java
+++ StateMachine/src/main/java/de/amr/statemachine/Match.java
@@ -1,7 +1,11 @@
 package de.amr.statemachine;
 
+/**
+ * Enumerates the event matching strategies of a state machine. Events are either matched by event
+ * object equality or by event class.
+ * 
+ * @author Armin Reichert
+ */
 public enum Match {
-
 	BY_EQUALITY, BY_CLASS
-
 }
