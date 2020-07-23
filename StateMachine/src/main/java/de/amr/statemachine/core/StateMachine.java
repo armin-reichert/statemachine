7b3c36b16b89c2336db093c1a0ad20e09ec8f11c
Armin Reichert <armin.reichert@web.de>
2020-07-23 07:49:40 +0200

    improved

--- StateMachine/src/main/java/de/amr/statemachine/core/StateMachine.java
+++ StateMachine/src/main/java/de/amr/statemachine/core/StateMachine.java
@@ -414,6 +414,7 @@ public class StateMachine<S, E> implements Fsm<S, E> {
 		restartTimer(stateId);
 		state(currentStateId).entryAction.run();
 		fireEntryListeners(currentStateId);
+		lastFiredTransition = null;
 	}
 
 	@Override
