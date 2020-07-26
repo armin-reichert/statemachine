810687dffd4e16ba4b8f2241a155ca5c3e28b50b
Armin Reichert <armin.reichert@web.de>
2020-07-26 11:37:42 +0200

    doc

--- StateMachine/src/main/java/de/amr/statemachine/api/TickAction.java
+++ StateMachine/src/main/java/de/amr/statemachine/api/TickAction.java
@@ -2,8 +2,21 @@ package de.amr.statemachine.api;
 
 import de.amr.statemachine.core.State;
 
+/**
+ * A transition action can get access to the state object and the timer values when the action is
+ * executed.
+ * 
+ * @author Armin Reichert
+ *
+ * @param <S> state identifier type
+ */
 @FunctionalInterface
 public interface TickAction<S> {
 
+	/**
+	 * @param state          the state before the state transition has been executed
+	 * @param ticksConsumed  ticks consumed in this state
+	 * @param ticksRemaining ticks remaining until tiemout
+	 */
 	void run(State<S> state, int ticksConsumed, int ticksRemaining);
 }
